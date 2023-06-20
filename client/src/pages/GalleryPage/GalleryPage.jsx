0ab071df550aaf9be0c040ab0acf04ab6f7d33bc
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-06-20 15:00:15 +0100

    add photoswipe delete react gallery

--- client/src/pages/GalleryPage/GalleryPage.jsx
+++ client/src/pages/GalleryPage/GalleryPage.jsx
@@ -1,47 +1,103 @@
-import "./GalleryPage.css";
-// import { useState } from "react";
-import { Gallery } from "react-grid-gallery";
-
-import { images } from "./components/Images";
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+// I disabled this until find a solution for them
+import { Gallery, Item } from "react-photoswipe-gallery";
+import "photoswipe/dist/photoswipe.css";
 
 const GalleryPage = () => {
-	// const [index, setIndex] = useState(-1);
+	const smallItemStyles = {
+		cursor: "pointer",
+		objectFit: "cover",
+		width: "100%",
+		maxHeight: "100%",
+	};
 
-	// const currentImage = images[index];
-	// const nextIndex = (index + 1) % images.length;
-	// const nextImage = images[nextIndex] || currentImage;
-	// const prevIndex = (index + images.length - 1) % images.length;
-	// const prevImage = images[prevIndex] || currentImage;
+	const items = [
+		{
+			original:
+				"https://farm4.staticflickr.com/3894/15008518202_c265dfa55f_h.jpg",
+			thumbnail:
+				"https://farm4.staticflickr.com/3894/15008518202_b016d7d289_m.jpg",
+			width: "1600",
+			height: "1600",
+			caption: "Photo of seashore by Folkert Gorter",
+		},
+		{
+			original:
+				"https://farm6.staticflickr.com/5591/15008867125_b61960af01_h.jpg",
+			thumbnail:
+				"https://farm6.staticflickr.com/5591/15008867125_68a8ed88cc_m.jpg",
+			width: "1600",
+			height: "1068",
+			caption: "Photo of mountain lake by Samuel Rohl",
+		},
+		{
+			original:
+				"https://farm4.staticflickr.com/3902/14985871946_86abb8c56f_b.jpg",
+			thumbnail:
+				"https://farm4.staticflickr.com/3902/14985871946_86abb8c56f_m.jpg",
+			width: "1600",
+			height: "1066",
+			caption: "Photo of fog in the village by Ales Krivec",
+		},
+		{
+			original:
+				"https://farm6.staticflickr.com/5584/14985868676_b51baa4071_h.jpg",
+			thumbnail:
+				"https://farm6.staticflickr.com/5584/14985868676_4b802b932a_m.jpg",
+			width: "1600",
+			height: "1066",
+			caption: "Photo of river sunset by Michael Hull",
+		},
+		{
+			original:
+				"https://farm4.staticflickr.com/3920/15008465772_d50c8f0531_h.jpg",
+			thumbnail:
+				"https://farm4.staticflickr.com/3920/15008465772_383e697089_m.jpg",
+			width: "1600",
+			height: "1066",
+			caption: "Photo of bear by Thomas Lefebvre",
+		},
+	];
 
-	const handleClick = (index) => setIndex(index);
-	// const handleClose = () => setIndex(-1);
-	// const handleMovePrev = () => setIndex(prevIndex);
-	// const handleMoveNext = () => setIndex(nextIndex);
+
 	return (
-		<>
-			<h1>Gallery</h1>
-			<div>
-				<Gallery
-					images={images}
-					onClick={handleClick}
-					enableImageSelection={false}
-				/>
-				{/* {!!currentImage && (
-					<Lightbox
-						mainSrc={currentImage.original}
-						imageTitle={currentImage.caption}
-						mainSrcThumbnail={currentImage.src}
-						nextSrc={nextImage.original}
-						nextSrcThumbnail={nextImage.src}
-						prevSrc={prevImage.original}
-						prevSrcThumbnail={prevImage.src}
-						onCloseRequest={handleClose}
-						onMovePrevRequest={handleMovePrev}
-						onMoveNextRequest={handleMoveNext}
-					/>
-				)} */}
+		<Gallery>
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: "240px 171px 171px",
+					gridTemplateRows: "114px 114px",
+					gridGap: 12,
+				}}
+			>
+				{items.map((item, index) => (
+					<Item
+						key={index}
+						original={item.original}
+						thumbnail={item.thumbnail}
+						width={item.width}
+						height={item.height}
+						caption={item.caption}
+					>
+						{({ ref, open }) => (
+							<img
+								style={
+									index === 0
+										? { ...smallItemStyles, cursor: "pointer" }
+										: smallItemStyles
+								}
+								src={item.thumbnail}
+								ref={ref}
+								onClick={open}
+								alt="picture"
+							/>
+						)}
+					</Item>
+				))}
 			</div>
-		</>
+		</Gallery>
 	);
 };
 
